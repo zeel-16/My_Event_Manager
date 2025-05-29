d17c9c115076502a45cdd5636382f3fd6d83d67f
codecrafter <simejiyazeel16@gmail.com>
2025-05-29 13:26:44 +0530

    Initial plugin setup and basic structure

---
+++ my-event-manager.php
@@ -0,0 +1,18 @@
+<?php
+/*
+Plugin Name: My Event Manager
+Plugin URI: https://yourwebsite.com/my-event-manager (You can update this later with your GitHub repo URL)
+Description: A simple WordPress plugin to manage and display events.
+Version: 1.0.0
+Author: Simejiya Zeel Rajenbhai
+License: GPL2
+*/
+
+// Exit if accessed directly.
+if ( ! defined( 'ABSPATH' ) ) {
+    exit;
+}
+
+// Your plugin's code will go here.
+// We'll add more in the next steps!
+?>
